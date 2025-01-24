fb7f8b195c0a3395ed94befd75cde7ad2b495ca4
Khakimov Doston <Khakimovmma12@gmail.com>
2025-01-24 23:58:24 +0500

    Task6

---
+++ Tyuiu.KhakimovDKh.Sprint5.Task6.V21.Test/DataServiceTest.cs
@@ -0,0 +1,34 @@
+﻿
+using Tyuiu.KhakimovDKh.Sprint5.Task6.V21.Lib;
+
+
+namespace Tyuiu.KhakimovDKh.Sprint5.Task6.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\Users\user\source\repos\Tyuiu.KhakimovDKh.Sprint5\DataSprint5\InPutDataFileTask6V21.txt";
+
+            var res = ds.LoadFromDataFile(path);
+            int wait = 2;
+
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void TestMethod()
+        {
+            string path = @"C:\Users\user\source\repos\Tyuiu.KhakimovDKh.Sprint5\DataSprint5\InPutDataFileTask6V21.txt";
+
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileexists = fileInfo.Exists;
+            bool expect = true;
+            Assert.AreEqual(expect, fileexists);
+        }
+    }
+}
---
+++ Tyuiu.KhakimovDKh.Sprint5.Task6.V21/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.KhakimovDKh.Sprint5.Task6.V21.Lib;
+
+
+
+namespace Tyuiu.KhakimovDKh.Sprint5.Task6.V21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Хакимов Д.Х. | ИИПб-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Обработка текстовых файлов                                        *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнил: Хакимов Достонбек Хисравович | ИИПб-24-2                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл (файл взять из архива согласно вашему варианту. Создать папку  *");
+            Console.WriteLine("* в ручную и скопировать в неё файл) в котором есть набор символьных      *");
+            Console.WriteLine("* данных. Найти количество удвоенных букв тт в заданной строке.           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИССХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("***************************************************************************");
+
+
+            string path = $@"{Directory.GetCurrentDirectory()}\InPutDataFileTask6V21.txt";
+
+            Console.WriteLine("Данные находяться в файле: " + path);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+
+        }
+    }
+}
